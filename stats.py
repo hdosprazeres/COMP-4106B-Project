daba14fa22c16a31cbaccebcba3913bd8d1149f8
Ryan Lubin <ryan.lubin1@gmail.com>
2021-04-16 09:16:42 -0400

    added perf comp graph

--- stats.py
+++ stats.py
@@ -2,6 +2,10 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+snake_ai_mmm = []
+snake_ai_names = ["snake_ai", "snake_ai_v4",
+                  "snake_ai_v5", "snake_ai_v6", "snake_ai_v7"]
+
 
 def run_stats(file_path, name):
     data = np.fromfile(file_path, dtype=int, sep=" ")
@@ -11,6 +15,7 @@ def run_stats(file_path, name):
     ax.set_xlabel("Score")
     ax.set_ylabel("Times achieved score")
     ax.set_title(name)
+    snake_ai_mmm.append([data.min(), data.mean(), data.max()])
     plt.axvline(data.min(), color='k', linestyle='dashed', linewidth=1)
     plt.axvline(data.mean(), color='k', linestyle='dashed', linewidth=1)
     plt.axvline(data.max(), color='k', linestyle='dashed', linewidth=1)
@@ -21,9 +26,23 @@ def run_stats(file_path, name):
     plt.show()
 
 
+def group_stats():
+    fig, ax = plt.subplots()
+    colors = []
+    for x in range(len(snake_ai_mmm)):
+        ax.plot(["Min", "Mean", "Max"], snake_ai_mmm[x],
+                label=snake_ai_names[x])
+    ax.set_ylabel("Score")
+    ax.set_title("Performance Comparison")
+    ax.legend()
+    plt.show()
+
+
 location = os.getcwd()
 score_files = os.listdir(os.path.abspath(os.path.join(location, "scores")))
 
 for score_file in score_files:
     path = os.path.abspath(os.path.join(location, "scores", score_file))
     run_stats(path, score_file)
+
+group_stats()
