67142477b99533df47c70a2d10409e8b66aac402
Ryan Lubin <ryan.lubin1@gmail.com>
2021-04-15 22:13:25 -0400

    min, mean, max added to stats.py histogram

--- stats.py
+++ stats.py
@@ -7,10 +7,17 @@ def run_stats(file_path, name):
     data = np.fromfile(file_path, dtype=int, sep=" ")
     # print(data)
     fig, ax = plt.subplots()
-    ax.hist(data)
+    ax.hist(data, color='c', edgecolor='k')
     ax.set_xlabel("Score")
     ax.set_ylabel("Times achieved score")
     ax.set_title(name)
+    plt.axvline(data.min(), color='k', linestyle='dashed', linewidth=1)
+    plt.axvline(data.mean(), color='k', linestyle='dashed', linewidth=1)
+    plt.axvline(data.max(), color='k', linestyle='dashed', linewidth=1)
+    min_ylim, max_ylim = plt.ylim()
+    plt.text(data.min()+1, max_ylim*0.9, 'Min: {:.2f}'.format(data.min()))
+    plt.text(data.mean()+1, max_ylim*0.9, 'Mean: {:.2f}'.format(data.mean()))
+    plt.text(data.max()-7, max_ylim*0.9, 'Max: {:.2f}'.format(data.max()))
     plt.show()
 
 
